722274ab1ba37a5c719ab6d43c37998d418f3ed2
PaulRomanov <pavel.ramanau@gmail.com>
2024-01-13 17:03:20 +0400

    create start page

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
 <template>
- <div>Memory game</div>
+ <StartPage/>
 </template>
 
 <script>
 
+import StartPage from "./components/StartPage.vue";
 
 export default {
   name: 'App',
   components: {
-   
+   StartPage
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/StartPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>The memory game</div>
+</template>
+
+<script>
+export default {
+  name: "StartPage",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
