e7515ccab08de24d83381604593f014410f114ce
PaulRomanov <pavel.ramanau@gmail.com>
2024-01-13 18:09:36 +0400

    add start page

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,9 @@
 </template>
 
 <style>
-
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 </style>
--- src/components/StartPage.vue
+++ src/components/StartPage.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>The memory game</div>
+  <div class="start-page">
+    <h1 class="start-page__title">
+      <span class="start-page__title-item">Memory</span>
+      game
+    </h1>
+    <p class="start-page__description">Click on the cards to find a match</p>
+    <button class="start-page__button">Start game</button>
+  </div>
 </template>
 
 <script>
@@ -14,4 +21,38 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="sass" scoped>
+@import "@/assets/variables/colors"
+.start-page
+  width: 100vw
+  height: 100vh
+  background-color: $primary-background-color
+  display: flex
+  justify-content: center
+  align-items: center
+  flex-direction: column
+  gap: 2rem
+
+  &__title
+    font-size: 3rem
+    color: $white-color
+    &-item
+      color: $primary-color
+
+  &__description
+    font-size: 1.5rem
+    color: $secondary-color
+
+  &__button
+    margin-top: 2rem
+    padding: 1rem 2rem
+    font-size: 1.5rem
+    background-color: $primary-color
+    border: none
+    border-radius: 0.5rem
+    cursor: pointer
+    transition: all 0.3s ease-in-out
+
+    &:hover
+      background-color: $primary-color-hover
+</style>
