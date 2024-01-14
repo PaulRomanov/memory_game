45702c27255d4b02baaffd46fe2eddf538511e0e
PaulRomanov <pavel.ramanau@gmail.com>
2024-01-14 20:26:06 +0400

    add store

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-createApp(App).use(router).mount('#app')
+window.__VUE_PROD_HYDRATION_MISMATCH_DETAILS__ = true;
+
+const app = createApp(App);
+
+app.use(router).use(store).mount('#app');
