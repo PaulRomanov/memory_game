e38e6b68d6fbd3265ab72c34e9e6c12c4d274bfe
PaulRomanov <pavel.ramanau@gmail.com>
2024-01-15 22:55:45 +0400

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <template>
-  <router-view/>
+  <AppHeader />
+  <router-view />
 </template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+  },
+};
+</script>
 
 <style>
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+}
+#app {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header">
+    <div class="header__wrapper-btns">
+      <button @click="changeLanguage('en')">English</button>
+      <button @click="changeLanguage('ru')">Russian</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  data() {
+    return {};
+  },
+  props: {},
+  beforeMount() {
+    if (!this.$i18n || !this.$i18n.global) {
+      console.error("$i18n or $i18n.global is not defined yet.");
+    }
+  },
+  methods: {
+    changeLanguage(locale) {
+      if (this.$i18n && this.$i18n.global) {
+        this.$i18n.global.locale = locale;
+      } else if (this.$i18n) {
+        this.$i18n.locale = locale;
+      } else {
+        console.error("$i18n or $i18n.global is not defined yet.");
+      }
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.header
+  width: 100%
+  background-color: $primary-background-color
+</style>
